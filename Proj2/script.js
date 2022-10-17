04799014d31e1a790d19c93834819708f77c63c6
lgancarczyk <79982113+lgancarczyk@users.noreply.github.com>
2022-10-17 21:23:35 +0200

    karuzela sie kreci

--- Proj2/script.js
+++ Proj2/script.js
@@ -1,28 +1,50 @@
-document.querySelector("#left-slider-button").addEventListener("click", moveRight);
+document.querySelector("#left-slider-button").addEventListener('click', moveLeft);
 document.querySelector("#right-slider-button").addEventListener("click", moveRight);
 
-const maxPX = 3000;
+const sliderSlide = document.querySelector('.slider-slide');
+const slides = document.querySelectorAll(".slide");
 
-// const newspaperSpinning = [
-//     { transform: maxPX + 600 },
-//     { transform: 'rotate(360deg) scale(0)' }
-//   ];
+const widthPx = 600;
+const slidesCount = 5;
+let counter = 0;
 
-//   function moveRight() {
-//        document.querySelectorAll(".slide").forEach(slide => console.log("www"))
-       
-//       }
-
-function moveRight() {
-   document.querySelectorAll(".slide").forEach(slide => {
-        slide.animate([
-            // keyframes
-            { transform: 'translateX(0px)' },
-            { transform: 'translateX(-600px)' }
-          ], {
-            // timing options
-            duration: 1000
-          });
-    });
-   
+function moveRight(){
+  sliderSlide.style.transition = "transform 0.6s ease-in-out";
+  if (counter <= slidesCount-2){
+    counter++;
+    sliderSlide.style.transform = `translateX(${ -widthPx * counter }px)`;
   }
+  else {
+    counter=0;
+    sliderSlide.style.transform = `translateX(${ -widthPx * counter }px)`;
+  }
+};
+
+function moveLeft(){
+  sliderSlide.style.transition = "transform 0.6s ease-in-out";
+  if (counter > 0){
+    counter--;
+    sliderSlide.style.transform = `translateX(${ -widthPx * counter }px)`;
+  }
+  else {
+    counter=slidesCount-1;
+    sliderSlide.style.transform = `translateX(${ -widthPx * counter }px)`;
+  }
+}
+
+// const timeoutRef = setTimeout( 
+//   () => {
+//       main.innerHTML='From setTimeout'
+//   },
+//   2000
+// )
+// let licznik = 0 
+// const intervalRef = setInterval( 
+//   () => {
+//       moveRight
+//   },
+//   2000
+// )
+
+const intervalId = window.setInterval(moveRight, 5000);
+
