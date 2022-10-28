7918d1d620d07057cf1052fe3ceb14a98be1abca
lgancarczyk <79982113+lgancarczyk@users.noreply.github.com>
2022-10-28 16:36:05 +0200

    Step I have to ask about

--- Proj2/script.js
+++ Proj2/script.js
@@ -1,12 +1,36 @@
 document.querySelector("#left-slider-button").addEventListener('click', moveLeft);
-document.querySelector("#right-slider-button").addEventListener("click", moveRight);
-
+document.querySelector("#right-slider-button").addEventListener("click", rightClick);
+const sliderNavButtonsContainer = document.querySelector('.slider-nav-buttons-container');
 const sliderSlide = document.querySelector('.slider-slide');
 const slides = document.querySelectorAll(".slide");
 
 const widthPx = 600;
 const slidesCount = 5;
 let counter = 0;
+
+const interval = window.setInterval(moveRight, 5000);
+
+setupSliderNavButtons();
+
+function setupSliderNavButtons(){
+  for (let index = 0; index < slidesCount; index++) {  
+    const el = document.createElement('button');
+    el.setAttribute('class', 'slide-nav-buutton');
+    el.setAttribute('id', `slideNavBuutton${index}`);
+    sliderNavButtonsContainer.appendChild(el);
+    el.addEventListener("click", moveToSpecificSlide(index));
+  }
+}
+
+function moveToSpecificSlide( index ){
+  console.log(index);
+}
+
+function rightClick(){
+  moveRight();
+  clearInterval(interval);
+  const interval = window.setInterval(moveRight, 5000);
+}
 
 function moveRight(){
   sliderSlide.style.transition = "transform 0.6s ease-in-out";
@@ -31,20 +55,3 @@ function moveLeft(){
     sliderSlide.style.transform = `translateX(${ -widthPx * counter }px)`;
   }
 }
-
-// const timeoutRef = setTimeout( 
-//   () => {
-//       main.innerHTML='From setTimeout'
-//   },
-//   2000
-// )
-// let licznik = 0 
-// const intervalRef = setInterval( 
-//   () => {
-//       moveRight
-//   },
-//   2000
-// )
-
-const intervalId = window.setInterval(moveRight, 5000);
-
