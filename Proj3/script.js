2891bbc1f06af088b1e71396900925d594172da5
lgancarczyk <79982113+lgancarczyk@users.noreply.github.com>
2022-11-07 23:08:15 +0100

    working recorder

--- Proj3/script.js
+++ Proj3/script.js
@@ -4,9 +4,54 @@ import {KeyToSound} from './Constants/KeyToSound.js';
 
 document.addEventListener('keypress', onKeyPress);
 const startStopButton = document.querySelector('#startStopRecordingButton')
-startStopButton.addEventListener('click', changeRecordingStatus)
+startStopButton.addEventListener('click', recordingButtonPressed)
 
 let isRecording = false;
+const records = [];
+const currentRecording = {
+    startTime: 0,
+    endTime: 0,
+    notes: []
+}
+
+function recordingButtonPressed(){
+    changeRecordingStatus();
+    startRecording();
+}
+
+function startRecording(){
+    currentRecording.notes = []
+    currentRecording.startTime = Date.now()
+}
+
+function endRecording(){
+    currentRecording.endTime = Date.now()
+
+    if (currentRecording.notes.length>0) {
+        const recordingWithTimeStamps = []
+
+        for (let i = 0; i < currentRecording.notes.length; i++) {
+            recordingWithTimeStamps.push([currentRecording.notes[i][0], currentRecording.notes[i][1]-currentRecording.startTime])
+        }
+
+        records.push(recordingWithTimeStamps)
+        displayRecord()
+    }
+}
+
+function displayRecord(){
+    const egzampleRecord = document.querySelector('#egzampleHiddenRecord')
+    const newRecord = egzampleRecord.cloneNode(true);
+    newRecord.setAttribute('id', `record-${records.length-1}` );
+    const newRecordButtonContainer = newRecord.querySelector('.recordContainerButtonsContainer')
+    const playButton = newRecordButtonContainer.querySelector('.playRecordButton')
+    playButton.setAttribute('id', `play-${records.length-1}`)
+    playButton.addEventListener('click', playRecord)
+    const deleteButton = newRecordButtonContainer.querySelector('.deleteRecordButton')
+    deleteButton.setAttribute('id', `delete-${records.length-1}`)
+    deleteButton.addEventListener('click', deleteRecord)
+    document.querySelector('.recordsContainer').appendChild(newRecord)
+}
 
 function changeRecordingStatus(){
     if (isRecording == false){
@@ -16,18 +61,20 @@ function changeRecordingStatus(){
     else{
         isRecording = false;
         startStopButton.setAttribute('class', 'buttonIsNotRecording' );
+        endRecording()
     }
 }
 
 function onKeyPress(ev) {
     const sound = KeyToSound[ev.key]
-    if(sound != undefined){
+    if(sound != undefined && isRecording == true){
         recordSound(sound);
     }
 }
 
 function recordSound(sound){
     playSound(sound);
+    currentRecording.notes.push([sound, Date.now()])
 }
 
 function playSound(sound) {
@@ -42,3 +89,18 @@ function playSound(sound) {
 function getSoundById(sound){
     return document.querySelector(`#${sound}`);
 }
+
+function deleteRecord(ev){
+    const record = document.querySelector(`#record-${(ev.target.id).slice(-1)}`)
+    record.remove()
+}
+
+function playRecord(ev){
+    const record = records[(ev.target.id).slice(-1)]
+    record.forEach(element => {
+        const delay = element[1]
+        setTimeout(function(){playSound(element[0])}, element[1])
+    });
+    console.log(record)
+}
+
